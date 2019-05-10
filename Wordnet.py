d2b0f107dfc96913ec52068d7c51d217157873d3
falboy1 <10haya22ton@gmail.com>
2019-05-10 19:39:57 +0900

    単語情報を出力する関数を作成

--- Wordnet.py
+++ Wordnet.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pprint
 
-# 類似単語を検索する関数
+# 類似単語を検索する関数(未完成)
 def search_similar_words(conn, query):
     # lemmaがqueryと一致した行のidのカーソルオブジェを取得
     cur = conn.execute("select wordid, pos from word where lemma='%s'" % query)
@@ -10,15 +10,41 @@ def search_similar_words(conn, query):
     # 
     return result
 
+# 単語の情報を検索する関数
+def search_word_info(conn, query):
+    # lemmaがqueryと一致した行のidのカーソルオブジェを取得
+    cur = conn.execute("select wordid, pos from word where lemma='%s'" % query)
+    # 一致結果の行を取得 (複数ヒットする可能性がある)
+    result = cur.fetchall()
+    # ヒット件数を表示
+    print(str(len(result)) + 'hit')
+    for t in result:
+        # 品詞を表示
+        print('%s:%s" of "%s"' % (t[0], t[1], query))
+        # 単語IDを取得
+        word_id = t[0]
+        # 概念IDを取得 (複数ヒットする可能性がある)
+        cur = conn.execute("select synset from sense where wordid='%s'" % word_id)
+        sense_result = cur.fetchall()
+        # ヒット件数を表示
+        print("%s hit" % len(sense_result))
+        for synset in sense_result:
+            print("synset:%s" % synset)
+            # 概念の意味を表示
+            cur = conn.execute("select pos, name from synset where synset='%s'" % synset)
+            synset_result = cur.fetchall()
+            print('pos="%s", name="%s"' % (synset_result[0][0], synset_result[0][1]))
+        print("\n")
+
+        
+
 def main():
     # ファイルパス
     filepath = 'data/wnjpn.db'
     # データベースにコネクト
     conn = sqlite3.connect(filepath)
     # カーソルを取得
-
-    x = search_similar_words(conn, '選挙')
-    print(x)
+    search_word_info(conn, '元気')
 
 if __name__ == '__main__':
     main()
