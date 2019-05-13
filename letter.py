0a5de956f75aaef93db1344c74df396e55770197
falboy1 <10haya22ton@gmail.com>
2019-05-14 01:44:14 +0900

    str形式のimisを辞書inリスト形式に変換する関数
    is_human_category：人、動物カテゴリに属するかのチェック

--- letter.py
+++ letter.py
@@ -1,5 +1,6 @@
 from pyknp import KNP
 import sys
+import pprint
 
 # creat compound noun(letter)
 def compound_noun(word, _knp):
@@ -15,27 +16,59 @@ def compound_noun(word, _knp):
 
     # 複合名詞を出力
 
+# 様々な解析結果(文字列)を辞書形式に変換する関数
+def get_dic(str):
+    pass
+
+# str形式のimisをlist[dic{'xxx'=yyy}]形式に変換する関数
+def convert_imis2dic(str):
+    str_list = str.split(" ")
+    result = []
+    for s in str_list:
+        i = s.find(":")
+        key = s[:i]
+        value = s[i+1:]
+        dic = {key: value}
+        result.append(dic)
+    return result
+
+# 辞書形式imis内に特定カテゴリが含まれるかチェック
+def is_human_category(_list):
+    flag = False
+    for d in _list:
+        if 'カテゴリ' in d:
+            if d['カテゴリ'] == '人' or d['カテゴリ'] == '動物':
+                flag = True
+    return flag
+
 # main method
 def main():
     # knp
     knp = KNP()
     # 解析
-    result = knp.parse("今日も元気です")
+    result = knp.parse("このバカ野郎が！")
 
     print("文節")
     for bnst in result.bnst_list():
-        print("\tID:%d, 見出し:%s, 係り受けタイプ:%s, 親文節ID:%d, 素性:%s" \
-                % (bnst.bnst_id, "".join(mrph.midasi for mrph in bnst.mrph_list()), bnst.dpndtype, bnst.parent_id, bnst.fstring))
+        print("\tID:%d, 見出し:%s, 係り受けタイプ:%s, 親文節ID:%d, 素性:%s" 
+            % (bnst.bnst_id, "".join(mrph.midasi for mrph in bnst.mrph_list()), bnst.dpndtype, bnst.parent_id, bnst.fstring))
 
     print("基本句")
     for tag in result.tag_list(): # 各基本句へのアクセス
-        print("\tID:%d, 見出し:%s, 係り受けタイプ:%s, 親基本句ID:%d, 素性:%s" \
-                % (tag.tag_id, "".join(mrph.midasi for mrph in tag.mrph_list()), tag.dpndtype, tag.parent_id, tag.fstring))
+        print("\tID:%d, 見出し:%s, 係り受けタイプ:%s, 親基本句ID:%d, 素性:%s" 
+            % (tag.tag_id, "".join(mrph.midasi for mrph in tag.mrph_list()), tag.dpndtype, tag.parent_id, tag.fstring))
 
     print("形態素")
     for mrph in result.mrph_list(): # 各形態素へのアクセス
-        print("\tID:%d, 見出し:%s, 読み:%s, 原形:%s, 品詞:%s, 品詞細分類:%s, 活用型:%s, 活用形:%s, 意味情報:%s, 代表表記:%s" \
-                % (mrph.mrph_id, mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
+        print("見出し:%s\n 読み:%s\n 原形:%s \n 品詞:%s \n 品詞細分類:%s \n 活用型:%s \n 活用形:%s \n 意味情報:%s \n 代表表記:%s" 
+        % (mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
+
+    for mrph in result.mrph_list():
+        imis = convert_imis2dic(mrph.imis)
+        print(is_human_category(imis))
+        pprint.pprint(imis)
+        print("\n")
+
 
 if __name__ == '__main__':
     main()
