ace1978de27ff75d8c8da3bb86d1d2976ccd582d
falboy1 <10haya22ton@gmail.com>
2019-05-13 18:59:21 +0900

    オプション引数で処理を分岐するように設定
    -w：単語情報の簡易検索を実行
    
    -s：概念定義の検索を実行

--- Wordnet.py
+++ Wordnet.py
@@ -2,14 +2,8 @@ import sqlite3
 import pprint
 import argparse
 
-# 類似単語を検索する関数(未完成)
-def search_similar_words(conn, query):
-    # lemmaがqueryと一致した行のidのカーソルオブジェを取得
-    cur = conn.execute("select wordid, pos from word where lemma='%s'" % query)
-    # 一致結果の行を取得
-    result = cur.fetchall()
-    # 
-    return result
+#-----------------------------------------
+# 開始ライン : 基本処理の関数
 
 # 上位語リストを入手する関数
 def get_hypernym(conn, query):
@@ -75,7 +69,7 @@ def get_hyponym_list(conn, synset_id):
         return result   
 
 # 概念IDから概念情報{'pos'=xxx, 'name'=yyy}を返す関数
-def get_synset(conn, synsetid):
+def get_synset_info(conn, synsetid):
     # 概念情報を取得
     cur = conn.execute("select pos, name from synset where synset='%s'" % synsetid)
     synset_result = cur.fetchall()
@@ -84,6 +78,18 @@ def get_synset(conn, synsetid):
     for synset in synset_result:
         dic = dict(pos=synset[0], name=synset[1])
         result.append(dic)
+    # 値を返却
+    return result
+
+# 概念名から概念IDリストを返す関数
+def get_synsetid_list_from_name(conn, name):
+    # synsetテーブルから情報を取り出す
+    cur = conn.execute("select synset from synset where name='%s'" % name)
+    synset_result = cur.fetchall()
+    # 返却用の空リストを作成
+    result = []
+    for synset in synset_result:
+        result.append(synset[0])
     # 値を返却
     return result
 
@@ -121,7 +127,7 @@ def get_word(conn, wordid):
     return word_result
 
 # 単語の属する概念IDリストを返す関数　(単語IDを使用)
-def get_synsetid_list(conn, wordid):
+def get_synsetid_list_from_wordid(conn, wordid):
     cur = conn.execute("select synset from sense where wordid='%s'" % wordid)
     sense_result = cur.fetchall()
     # 空リスト作成
@@ -129,6 +135,10 @@ def get_synsetid_list(conn, wordid):
     for sense in sense_result:
         result.append(sense[0])
     return result
+
+# 終了ライン : 基本処理の関数
+# ------------------------------------------------
+# 開始ライン : 基本処理を使った出力までを行う関数
 
 # 単語の情報を検索する関数
 def search_word_info(conn, query):
@@ -147,13 +157,13 @@ def search_word_info(conn, query):
         # 単語IDを取得
         word_id = d['wordid']
         # 単語の属する概念ID一覧を取得 (複数ヒットする可能性がある)
-        synsetid_list = get_synsetid_list(conn, word_id)
+        synsetid_list = get_synsetid_list_from_wordid(conn, word_id)
         # ヒット件数を表示
         print("%s hit" % len(synsetid_list))
         for synsetid in synsetid_list:
             print("synsetid:%s" % synsetid)
             # 概念を取得
-            synset = get_synset(conn, synsetid)
+            synset = get_synset_info(conn, synsetid)
             # 取得した概念を表示
             for s in synset:
                 print("pos:%s, name:%s" % (s['pos'], s['name']))
@@ -165,19 +175,44 @@ def search_word_info(conn, query):
                 print("lang:%s, def:%s" % (d['lang'], d['define'][:25])) # 長い時があるから25文字でスライス
         print('\n')
 
+# 上位語を見ていく関数
+# def hypernym_loop(conn, query):
+
+# 概念名の定義を出力する関数
+def search_synset_def(conn, query):
+    list1 = get_synsetid_list_from_name(conn, query)
+    for d in list1:
+        print("synset_id:%s" % d)
+        define = get_synset_def(conn, d)
+        for d2 in define:
+            print("def:%s" % d2)
+        print("\n")
+
 def main():
     # ファイルパス
     filepath = 'data/wnjpn.db'
     # データベースにコネクト
     conn = sqlite3.connect(filepath)
+
     # argparseのオブジェクトを作成
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="検索クエリとオプションを指定して実行")
+
     # パラメータを追加
-    parser.add_argument("query")
+    parser.add_argument("query", help="検索クエリー")
+    parser.add_argument("-w", "--word_info", help="単語情報の検索")
+    parser.add_argument("-s", "--synset_def", help="概念説明の検索")
+
     # 引数を取得
     args = parser.parse_args()
-    # カーソルを取得
-    search_word_info(conn, args.query)
+
+    # 単語情報の簡易検索
+    if (args.word_info is not None):
+        search_word_info(conn, args.query)
+
+    # 概念定義を検索
+    if (args.synset_def is not None):
+        search_synset_def(conn, args.query)
+
 
 if __name__ == '__main__':
     main()
